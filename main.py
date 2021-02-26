018c6954cba1f32a2092c3d6ae15040841b91f1b
GlavRab <alek.bar.ru@yandex.ru>
2021-02-26 23:07:02 +0300

    refactor: move out handlers to library.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from uuid import uuid4
 from library import parsing_data, send_message, \
-                    adding_a_dog, working_with_command, \
-                    changing_a_dog, looking_for_a_dog
+                    working_with_command, delete_dog, \
+                    show_all_dogs, add_new_dog, \
+                    changing_info_about_dog
 
 
 app = Flask(__name__)
@@ -37,50 +37,13 @@ def index() -> dict:
         send_message(data)
         return data
     elif data['text'] == '/dogs':
-        data['text'] = str(users_dogs)
-        send_message(data)
-        return data
+        show_all_dogs(data=data, data_base=users_dogs)
     elif working_with_command(data['text'])[0] == '/add_dog':
-        new_dog = adding_a_dog(working_with_command(data['text']))
-        if new_dog != {}:
-            users_dogs[str(uuid4())] = new_dog
-            data['text'] = 'Собака успешно добавлена!'
-            send_message(data)
-            return new_dog
-        data['text'] = 'Не хватает аргументов'
-        send_message(data)
-        return users_dogs
+        add_new_dog(data=data, data_base=users_dogs)
     elif working_with_command(data['text'])[0] == '/del_dog':
-        info = working_with_command(data['text'])
-        name = info[1]
-        user_dog_id = looking_for_a_dog(data=users_dogs, name=name)
-        try:
-            if user_dog_id != 'Такой собаки нет(':
-                del users_dogs[user_dog_id]
-                data['text'] = 'Собака успешно удалена!'
-                send_message(data)
-                return users_dogs
-        except KeyError:
-            data['text'] = 'Такой собаки нет('
-            send_message(data)
+        delete_dog(data=data, data_base=users_dogs)
     elif working_with_command(data['text'])[0] == '/change_info':
-        info = working_with_command(data['text'])
-        old_name = info[1]
-        user_dog_id = looking_for_a_dog(data=users_dogs, name=old_name)
-        try:
-            if user_dog_id != 'Такой собаки нет(':
-                user_dog = changing_a_dog(info)
-                if user_dog != {}:
-                    users_dogs[user_dog_id] = user_dog
-                    data['text'] = 'Информация о собаке изменена!'
-                    send_message(data)
-                    return user_dog
-                elif user_dog == {}:
-                    data['text'] = 'Не хватает аргументов'
-                    send_message(data)
-        except KeyError:
-            data['text'] = 'Такой собаки нет('
-            send_message(data)
+        changing_info_about_dog(data=data, data_base=users_dogs)
     return data
 
 
